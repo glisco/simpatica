10f44ab962cef1b35f036667d0a6180a2b179188
Michele Comitini <mcm@glisco.it>
2011-11-25 22:36:57 +0100

    fixing problems

--- controllers/ca.py
+++ controllers/ca.py
@@ -1,4 +1,24 @@
 # -*- coding: utf-8 -*-
+#
+# simpatiCA - a simple PKI
+#
+# Copyright (C) 2011 by Michele Comitini <michele.comitini _at_ glisco.it>
+# Copyright (C) 2011 by Glisco s.r.l. <info _at_ glisco.it>
+#
+#     This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# see LICENSE for licensing info.
 
 #
 # Action related to the CA part of the PKI
--- controllers/default.py
+++ controllers/default.py
@@ -1,20 +1,33 @@
 # -*- coding: utf-8 -*-
+#
+# simpatiCA - a simple PKI
+#
+# Copyright (C) 2011 by Michele Comitini <michele.comitini _at_ glisco.it>
+# Copyright (C) 2011 by Glisco s.r.l. <info _at_ glisco.it>
+#
+#     This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# see LICENSE for licensing info.
 # this file is released under public domain and you can use without limitations
 
-#########################################################################
-## This is a samples controller
-## - index is the default action of any application
-## - user is required for authentication and authorization
-## - download is for downloading files uploaded in the db (does streaming)
-## - call exposes all registered services (none by default)
-#########################################################################
 
 def index():
     """
     example action using the internationalization operator T and flash
     rendered by views/default/index.html or views/generic.html
     """
-    return dict(message=T('Hello World'))
+    return dict()
 
 def user():
     """
